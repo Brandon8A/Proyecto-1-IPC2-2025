d9e391d3bc6a1cd9bfe5f0ff556cc02c955546c1
brandon8a <brandonochoa202030023@cunoc.edu.gt>
2025-03-06 23:26:10 -0600

    Creando Proyecto

---
+++ src/main/java/com/mycompany/proyecto1_ipc2_2025/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package com.mycompany.proyecto1_ipc2_2025;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
