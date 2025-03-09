6568e16c9eab4a203a4589ee373ac7ce0198616b
brandon8a <brandonochoa202030023@cunoc.edu.gt>
2025-03-09 17:49:07 -0600

    Arreglando inicio de sesion en servlet

---
+++ src/main/java/com/mycompany/proyecto1_ipc2_2025/resources/controller/LoguearUsuario.java
@@ -0,0 +1,117 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package com.mycompany.proyecto1_ipc2_2025.resources.controller;
+
+import com.mycompany.proyecto1_ipc2_2025.resources.conexiondb.ConexionDB;
+import com.mycompany.proyecto1_ipc2_2025.resources.encriptacion.EncriptarMD5;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author brandon
+ */
+@WebServlet(name = "LoguearUsuario", urlPatterns = {"/LoguearUsuario"})
+public class LoguearUsuario extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        /*response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet LoguearUsuario</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet LoguearUsuario at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }*/
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //processRequest(request, response);
+        ConexionDB conexionDB = new ConexionDB();
+        Statement statement = null;
+        ResultSet resultSet = null;
+        EncriptarMD5 encriptar = new EncriptarMD5();
+
+        if (request.getParameter("login") != null) {
+            String user = request.getParameter("user");
+            String password = request.getParameter("password");
+            HttpSession sesion = request.getSession();
+            try {
+                statement = conexionDB.getConnection().createStatement();
+                resultSet = statement.executeQuery("SELECT * FROM Usuario WHERE nombre_usuario = '" + user + "' AND password = '" + encriptar.getMD5(password) + "';");
+                while (resultSet.next()) {
+                    sesion.setAttribute("logueado", "1");
+                    sesion.setAttribute("user", resultSet.getString("nombre_usuario"));
+//                    sesion.setAttribute("id", resultSet.getString("id"));
+                    String rolUsuario = resultSet.getString("tipo_rol_fk");
+                    System.out.println("Rol: " + rolUsuario);
+                    if (rolUsuario.equals("3")) {
+                        response.sendRedirect("Vista/panelAdministracion.jsp");
+                    } else {
+                        response.sendRedirect("index.jsp");
+                    }
+                }
+                System.out.print("<div class=\"alert alert-danger\" role=\"alert\"> Usuario no valido</div>");
+            } catch (Exception e) {
+                System.out.print(e);
+            }
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -24,7 +24,7 @@
     <body class="flex items-center justify-center min-h-screen bg-gray-100">
         <div class="w-full max-w-md p-6 bg-white shadow-lg rounded-lg">
             <h2 class="text-2xl font-bold text-center text-gray-800 mb-6">Iniciar Sesión</h2>
-            <form action="index.jsp" method="post">
+            <form action="${pageContext.servletContext.contextPath}/LoguearUsuario" method="post">
                 <div class="mb-4">
                     <label class="block text-gray-700 font-medium mb-2" for="user">Usuario</label>
                     <input type="text" id="user" name="user" required class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500" placeholder="Usuario">
@@ -36,9 +36,9 @@
                 <div class="flex justify-between items-center mb-4">
                     <a href="Vista/RegistrarUsuario.jsp" class="text-sm text-purple-500 hover:underline">Registrarse</a>
                 </div>
-                <button type="submit" class="w-full bg-purple-600 hover:bg-purple-700 text-white font-medium py-2 rounded-lg transition">Iniciar Sesión</button>
+                <button name="login" type="submit" class="w-full bg-purple-600 hover:bg-purple-700 text-white font-medium py-2 rounded-lg transition">Iniciar Sesión</button>
             </form>
-            <%
+            <%--
                         final String URL_MYSQL = "jdbc:mysql://localhost:3306/Tech_Solutions_Hub";
                         final String USER = "root";
                         final String PASSWORD = "brandon031200";
@@ -67,7 +67,7 @@
                             out.print(e);
                             }
                         }
-                    %>
+                    --%>
         </div>
     </body>
 </body>
